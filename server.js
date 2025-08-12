f5e16cf3a26e88968937cde08a72debf80ed20fd
phopha <148693028+phopha@users.noreply.github.com>
2025-08-12 22:33:06 +0700

    finish day 3

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('hello world');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.send(`hello ${req.params.name}`);
+})
+
+app.get('/hello/:name/:age', (req, res) => {
+  const name = req.params.name;
+  const age = req.params.age;
+  res.send(`name = ${name}, age = ${age}`);
+})
+
+app.post('/hello', (req, res) => {
+  res.send(req.body);
+})
+
+app.put('/myPut', (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(3000, 'localhost', () => {
