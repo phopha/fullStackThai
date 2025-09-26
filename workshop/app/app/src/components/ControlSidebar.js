3764283f564ecf3757df5ff60257a93b5b3ee74d
phopha <148693028+phopha@users.noreply.github.com>
2025-09-26 16:37:20 +0700

    finish at day 12

---
+++ workshop/app/app/src/components/ControlSidebar.js
@@ -0,0 +1,10 @@
+function ControlSidebar() {
+  return (
+    <>
+      <aside class="control-sidebar control-sidebar-dark">
+      </aside>
+    </>
+  )
+}
+
+export default ControlSidebar;
---
+++ workshop/app/app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import SingIn from './pages/backoffice/SingIn';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SingIn />
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
+);
+
+
